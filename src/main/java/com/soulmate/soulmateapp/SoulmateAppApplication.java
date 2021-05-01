db060aeccf1b8327d35f79a64e3af8844b4ee3c8
vuyyuruarvr <vuyyuruarvr@gmail.com>
2021-05-01 22:26:22 +0530

    more tc's added

---
+++ src/main/java/com/soulmate/soulmateapp/SoulmateAppApplication.java
@@ -0,0 +1,14 @@
+package com.soulmate.soulmateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulmateAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SoulmateAppApplication.class, args);
+	}
+
+}
